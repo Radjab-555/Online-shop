e21d54e88805015c77edb0e12a8cfe564335d039
Radzhab Alibekov <radzabalibecov77@gmail.com>
2023-02-27 17:58:30 +0300

    без ком

---
+++ min.js
@@ -0,0 +1,24 @@
+//  product image slideer 
+ var productThumb = new Swiper('.small-image', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        481: {
+            spaceBetween: 32,
+        }
+    }
+});
+var productBig = new Swiper ('.big-image', {
+    loop: true,
+    autoHeight: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+     thumbs: {
+        swiper: productThumb,
+    }
+})
