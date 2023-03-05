48f808fc59c702e0fa7b8d6bd7502dcfddff1c68
Radzhab Alibekov <radzabalibecov77@gmail.com>
2023-03-05 22:35:49 +0300

    добавлял слайдер к первому товару

--- min.js
+++ min.js
@@ -1,3 +1,24 @@
+// shov sub menu on mobile 
+// const submenu = document.querySelectorAll('.has-child .icon-small');
+// submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+// function toggle(e) {
+//     e.preventDefault();
+//     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+//     if (this.closest('.has-child').classList != 'expand');
+//     this.closest('.has-child').classList.thumbs('expand')
+// }
+
+// slider
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    
+    pagination: {
+        el: '.swiper-pagination',
+    }
+
+});
+
 //  product image slideer 
  var productThumb = new Swiper('.small-image', {
     loop: true,
@@ -21,4 +42,4 @@ var productBig = new Swiper ('.big-image', {
      thumbs: {
         swiper: productThumb,
     }
-})
+});
