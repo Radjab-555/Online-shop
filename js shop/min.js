0c6f1b7942275299f0635cf3e35e2f4a8baabe8a
Radzhab Alibekov <radzabalibecov77@gmail.com>
2023-11-21 21:34:19 +0300

    Добавил страницу оплаты

--- js shop/min.js
+++ js shop/min.js
@@ -93,26 +93,4 @@ function timeConverter(UNIX_timestamp) {
     return time;
 }
 
-
-
-
-
-
-
-
-// Каталог. Отображение товаров
-// Утилиты
-// function toNum(str) {
-//   const num = Number(str.replace(/ /g, ""));
-//   return num;
-// }
-
-// function toCurrency(num) {
-//   const format = new Intl.NumberFormat("ru-RU", {
-//     style: "currency",
-//     currency: "RUB",
-//     minimumFractionDigits: 0,
-//   }).format(num);
-//   return format;
-// }
   
